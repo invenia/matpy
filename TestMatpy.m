71ad996a6385b02be206ab1a4dd003d76f3ff05d
Samuel Massinon <samuelmassinon@Samuels-MBP.invenia>
2015-05-08 14:47:56 -0500

    Made a test for String Import and fix the assignin to assign to the callers workspace

--- TestMatpy.m
+++ TestMatpy.m
@@ -1,1 +1,26 @@
-% Stub File
+
+
+
+
+%% Test String Import
+function TestStringImport
+
+    symbols = ['a':'z' 'A':'Z' '0':'9'];
+    MAX_ST_LENGTH = 50;
+    strLength = randi(MAX_ST_LENGTH);
+    nums = randi(numel(symbols),[1 strLength]);
+    expected = symbols (nums);
+
+    stmt = sprintf([    
+                        'actual = "' ...
+                        expected ...
+                        '"\n'
+                    ]);
+
+    py('eval', stmt);
+
+    py_import actual
+
+    assertEqual(expected, actual, 'string import not successful')
+
+end
